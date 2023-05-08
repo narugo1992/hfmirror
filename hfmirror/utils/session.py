64637eeac63b66182733b73d5455f28ba9ac6a39
narugo1992 <narugo@126.com>
2023-05-08 11:14:51 +0800

    dev(narugo): update retry condition

--- hfmirror/utils/session.py
+++ hfmirror/utils/session.py
@@ -28,9 +28,8 @@ def get_requests_session(max_retries: int = 5, timeout: int = DEFAULT_TIMEOUT,
     session = requests.session()
     retries = Retry(
         total=max_retries, backoff_factor=1,
-        # status_forcelist=[500, 501, 502, 503, 504, 505, 506, 507, 509, 510, 511],
-        status_forcelist=[429, 500, 502, 503, 504],
-        allowed_methods=["HEAD", "GET", "OPTIONS"],
+        status_forcelist=[413, 429, 500, 501, 502, 503, 504, 505, 506, 507, 509, 510, 511],
+        allowed_methods=["HEAD", "GET", "POST", "PUT", "DELETE", "OPTIONS", "TRACE"],
     )
     adapter = TimeoutHTTPAdapter(max_retries=retries, timeout=timeout)
     session.mount('http://', adapter)
