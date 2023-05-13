89540ed40f2e8ea221ea475a0ad2f91101e6f112
narugo1992 <narugo@126.com>
2023-05-13 21:01:57 +0800

    dev(narugo): fix bug about sourceforge

--- test/resource/test_sourceforge.py
+++ test/resource/test_sourceforge.py
@@ -5,7 +5,7 @@ import pytest
 from hfmirror.resource import SourceForgeFilesResource
 
 
-class CheckstyleMirrorResource(SourceForgeFilesResource):
+class CustomMirrorResource(SourceForgeFilesResource):
     def _get_version(self, type_, segments) -> Optional[str]:
         if len(segments) == 1:
             return segments[0]
@@ -13,7 +13,7 @@ class CheckstyleMirrorResource(SourceForgeFilesResource):
             return None
 
     def _process_segments(self, type_, segments) -> Optional[List[str]]:
-        if segments[0] > 'checkstyle-10.9.0':
+        if segments[0] == 'v0.0.3':
             return None
         else:
             return segments
@@ -22,53 +22,52 @@ class CheckstyleMirrorResource(SourceForgeFilesResource):
 @pytest.mark.unittest
 class TestResourceSourceforge:
     def test_mirror_resource(self):
-        resource = CheckstyleMirrorResource('checkstyle.mirror')
+        resource = CustomMirrorResource('dghs-imgutils')
         tree = resource.sync_tree()
         assert 'LATEST_RELEASE' in tree.items
-        assert 'LATEST_RELEASE_10' in tree.items
-        assert 'LATEST_RELEASE_10.9.0' in tree.items
-        assert 'LATEST_RELEASE_10.9.1' not in tree.items
+        assert 'LATEST_RELEASE_0' in tree.items
+        assert 'LATEST_RELEASE_0.0.2' in tree.items
+        assert 'LATEST_RELEASE_0.0.3' not in tree.items
 
-        assert tree.items['LATEST_RELEASE_10.9'].content == 'checkstyle-10.9.0'
-        assert tree.items['checkstyle-10.9.0'].items['README.md'].refresh_mark(None) == {
-            "url": "https://downloads.sourceforge.net/project/checkstyle.mirror/checkstyle-10.9.0/README.md",
-            "etag": "\"6413020d-2a9\"",
-            "expires": None,
-            "content_length": 681,
-            "content_type": "application/octet-stream"
+        assert tree.items['LATEST_RELEASE_0'].content.startswith('v0.0.')
+        assert tree.items['LATEST_RELEASE_0'].content != 'v0.0.3'
+        assert tree.items['v0.0.2'].items['README.md'].refresh_mark(None) == {
+            'url': 'https://downloads.sourceforge.net/project/dghs-imgutils/v0.0.2/README.md',
+            'etag': '"6451fdc9-12f"',
+            'expires': None,
+            'content_length': 303,
+            'content_type': 'application/octet-stream'
         }
-        assert tree.items['checkstyle-10.9.0'].items['checkstyle-10.9.0-all.jar'].refresh_mark(None) == {
-            "url": "https://downloads.sourceforge.net/project/"
-                   "checkstyle.mirror/checkstyle-10.9.0/checkstyle-10.9.0-all.jar",
-            "etag": "\"64130497-fe8110\"",
-            "expires": None,
-            "content_length": 16679184,
-            "content_type": "application/java-archive"
+        assert tree.items['v0.0.2'].items['dghs_imgutils-0.0.2-py3-none-any.whl'].refresh_mark(None) == {
+            'url': 'https://downloads.sourceforge.net/project/dghs-imgutils/'
+                   'v0.0.2/dghs_imgutils-0.0.2-py3-none-any.whl',
+            'etag': '"6451fedf-a6d5"',
+            'expires': None,
+            'content_length': 42709,
+            'content_type': 'application/octet-stream'
         }
 
     def test_native(self):
-        resource = SourceForgeFilesResource('checkstyle.mirror')
+        resource = SourceForgeFilesResource('dghs-imgutils')
         tree = resource.sync_tree()
-        print(tree.items)
         assert 'LATEST_RELEASE' not in tree.items
-        assert 'LATEST_RELEASE_10' not in tree.items
-        assert 'LATEST_RELEASE_10.9.0' not in tree.items
-        assert 'LATEST_RELEASE_10.9.1' not in tree.items
-        assert 'LATEST_RELEASE_10.9.2' not in tree.items
-        assert 'LATEST_RELEASE_10.9.3' not in tree.items
+        assert 'LATEST_RELEASE_0' not in tree.items
+        assert 'LATEST_RELEASE_0.0.2' not in tree.items
+        assert 'LATEST_RELEASE_0.0.3' not in tree.items
+        assert 'LATEST_RELEASE_100' not in tree.items
 
-        assert tree.items['checkstyle-10.9.0'].items['README.md'].refresh_mark(None) == {
-            "url": "https://downloads.sourceforge.net/project/checkstyle.mirror/checkstyle-10.9.0/README.md",
-            "etag": "\"6413020d-2a9\"",
-            "expires": None,
-            "content_length": 681,
-            "content_type": "application/octet-stream"
+        assert tree.items['v0.0.2'].items['README.md'].refresh_mark(None) == {
+            'url': 'https://downloads.sourceforge.net/project/dghs-imgutils/v0.0.2/README.md',
+            'etag': '"6451fdc9-12f"',
+            'expires': None,
+            'content_length': 303,
+            'content_type': 'application/octet-stream'
         }
-        assert tree.items['checkstyle-10.9.0'].items['checkstyle-10.9.0-all.jar'].refresh_mark(None) == {
-            "url": "https://downloads.sourceforge.net/project/"
-                   "checkstyle.mirror/checkstyle-10.9.0/checkstyle-10.9.0-all.jar",
-            "etag": "\"64130497-fe8110\"",
-            "expires": None,
-            "content_length": 16679184,
-            "content_type": "application/java-archive"
+        assert tree.items['v0.0.2'].items['dghs_imgutils-0.0.2-py3-none-any.whl'].refresh_mark(None) == {
+            'url': 'https://downloads.sourceforge.net/project/dghs-imgutils/'
+                   'v0.0.2/dghs_imgutils-0.0.2-py3-none-any.whl',
+            'etag': '"6451fedf-a6d5"',
+            'expires': None,
+            'content_length': 42709,
+            'content_type': 'application/octet-stream'
         }
